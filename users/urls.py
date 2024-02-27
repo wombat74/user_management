4fbb6f9615d954ed3398dc5ce2020c703aef04bf
Eddie G <eddie.gonzalez@gmail.com>
2024-02-26 16:41:34 -0800

    first push of existing files to repo

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+# users/urls.py
+
+from django.urls import path, include
+from users.views import dashboard, register
+
+urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("dashboard/", dashboard, name="dashboard"),
+    path("register/", register, name="register"),
+]
